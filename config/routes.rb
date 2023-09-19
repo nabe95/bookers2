1b1b13c1d129c2faed06366a21844876bd5f7743
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2023-09-19 09:48:29 +0000

    [add] 一覧　編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "homes#top"
+  devise_for :users
+
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy, ]
+
+  get '/homes/about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
