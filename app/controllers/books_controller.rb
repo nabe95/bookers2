e68238ab4004deba9a74091db634029d680d648c
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2023-09-20 12:37:47 +0000

    [add] 部分テンプレート

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,28 +12,33 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @user = current_user
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @book_new = Book.new
+
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to '/books'
   end
-  
+
   private
 
   def book_params
